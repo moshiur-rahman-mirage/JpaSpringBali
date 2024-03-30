ab386a52c49b43559d10c65ed91fc4c73e46f1c1
Moshiur.Rahman <moshiur.mirage@gmail.com>
2024-03-30 21:34:09 +0600

    Initial Commit

---
+++ src/main/java/dev/moshiur/JpaSpringBali/JpaSpringBaliApplication.java
@@ -0,0 +1,13 @@
+package dev.moshiur.JpaSpringBali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpringBaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSpringBaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/moshiur/JpaSpringBali/JpaSpringBaliApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.moshiur.JpaSpringBali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpringBaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
