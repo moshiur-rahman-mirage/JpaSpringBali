86981177c8216773341948dfd757e3b8828d5457
Moshiur.Rahman <moshiur.mirage@gmail.com>
2024-03-31 22:16:02 +0600

    Initial Commit

--- src/main/java/dev/moshiur/JpaSpringBali/JpaSpringBaliApplication.java
+++ src/main/java/dev/moshiur/JpaSpringBali/JpaSpringBaliApplication.java
@@ -2,12 +2,20 @@ package dev.moshiur.JpaSpringBali;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaSpringBaliApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpaSpringBaliApplication.class, args);
+		var ctx = SpringApplication.run(JpaSpringBaliApplication.class, args);
+		MyFirstClass myFirstClass= ctx.getBean(MyFirstClass.class);
+		System.out.println(myFirstClass.sayHello());
 	}
+
+//	@Bean
+//	public MyFirstClass myFirstClass(){
+//		return new MyFirstClass();
+//	}
 
 }
---
+++ src/main/java/dev/moshiur/JpaSpringBali/MyFirstClass.java
@@ -0,0 +1,10 @@
+package dev.moshiur.JpaSpringBali;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFirstClass {
+    public String sayHello(){
+        return "Hello from Here";
+    }
+}
